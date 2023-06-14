498d6299ec7fc251aaee728b90b4278fc40b2477
Shute <31872129+ShuteLee@users.noreply.github.com>
2023-06-14 15:34:41 +0800

    1

--- utils.sh
+++ utils.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# function sync_repository() {
-#     rep_name=$1
-#     rep_url=$2
-#     mkdir -p $GIT_REPOSTORY_PATH
-#     pushd $GIT_REPOSTORY_PATH
-#     if [ -d $rep_name ]; then
-#         pushd $rep_name && git pull & git submodule update & popd
-#     else
-#         git clone $rep_url
-#     fi
-#     popd
-#     git clone $GIT_REPOSTORY_PATH/$rep_name
-# }
+function sync_repository() {
+    rep_name=$1
+    rep_url=$2
+    mkdir -p $GIT_REPOSTORY_PATH
+    pushd $GIT_REPOSTORY_PATH
+    if [ -d $rep_name ]; then
+        pushd $rep_name && git pull & git submodule update & popd
+    else
+        git clone $rep_url
+    fi
+    popd
+    git clone $GIT_REPOSTORY_PATH/$rep_name
+}
 
-# export GIT_REPOSTORY_PATH=/var/jenkins_home/git-repository
+export GIT_REPOSTORY_PATH=/var/jenkins_home/git-repository
